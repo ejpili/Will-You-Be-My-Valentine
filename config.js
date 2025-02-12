c9fcfe49dabb747c9722403ab72c2008f6af618c
ejpili <e.pili@hotmail.com>
2025-02-11 23:57:07 -0600

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "I wanted to ask you.. 💌",
+    pageTitle: "I wanted to ask you 💌",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
