7717a4e30a45610a2acda244b038547741a48efa
ejpili <e.pili@hotmail.com>
2025-02-11 23:37:53 -0600

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dgprtc21g/video/upload/v1739336079/Musiq_Soulchild_-_Halfcrazy_xbgyfz.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dgprtc21g/video/upload/v1739338611/Love_yoisgp.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
